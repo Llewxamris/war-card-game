8eba4b24ce984cf427ff232e38aa19e0118c3e34
Llewxamris <maxwell.r.haley@gmail.com>
2019-07-14 22:23:24 -0400

    feat(player): add player class
    player class is not yet used in the game. will be used during core
    gameplay refactoring.

---
+++ src/main/java/com/maxwellhaley/war/exception/NotEnoughCashException.java
@@ -0,0 +1,20 @@
+package com.maxwellhaley.war.exception;
+
+import com.maxwellhaley.war.model.Player;
+
+/**
+ * An exception that is raised whenever a user makes a {@link Player#bet(int)}
+ * that is greater than the same users {@link Player#cash}.
+ * 
+ * @author Maxwell Haley
+ * @version 2.0.0
+ * @since 2019-07-14
+ */
+public class NotEnoughCashException extends Exception {
+
+  /** Generated serial version UID */
+  private static final long serialVersionUID = 8173365003397522456L;
+
+  public NotEnoughCashException() {}
+
+}
