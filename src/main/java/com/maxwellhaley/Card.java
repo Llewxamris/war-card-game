164bb58e7e0853c24da6d263141fe5389f365ca9
Llewxamris <maxwell.r.haley@gmail.com>
2019-07-14 20:02:10 -0400

    refactor(card): add javadocs

--- src/main/java/com/maxwellhaley/Card.java
+++ src/main/java/com/maxwellhaley/Card.java
@@ -1,19 +1,45 @@
 package com.maxwellhaley;
 
+/**
+ * Represents a single card during a game of WAR!. A Card has two attributes: a
+ * {@link Rank}, and a {@link Suit}. The Joker card is not present in the game
+ * of WAR!.
+ * 
+ * Cards are stored as a collection inside a {@link Deck}.
+ * 
+ * @author Maxwell Haley
+ * @version 2.0.0
+ * @since 2016-11-03
+ */
 public class Card {
+  /** The cards rank */
   private Rank rank;
+  /** The cards suit */
   private Suit suit;
 
+  /**
+   * The cards rank and suit are assigned at creation. During normal play, there
+   * is no reason to change either value.
+   * 
+   * @param rank - The cards assigned rank
+   * @param suit - The cards assigned suit
+   */
   public Card(Rank rank, Suit suit) {
     this.rank = rank;
     this.suit = suit;
   }
 
-  protected Rank getRank() {
+  /**
+   * @return The cards rank
+   */
+  public Rank getRank() {
     return rank;
   }
 
-  protected Suit getSuit() {
+  /**
+   * @return The cards suit
+   */
+  public Suit getSuit() {
     return suit;
   }
 }
