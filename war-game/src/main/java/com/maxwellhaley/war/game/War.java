dd839ba5d69de75441610d415fca69fc953a13fe
Llewxamris <maxwell.r.haley@gmail.com>
2019-09-07 18:24:18 -0400

    Move away from TUI
    Going to move to a CLI for the sake of speed. Doing a full TUI with an
    unfamiliar library is just too much commitment for a casual project.

--- war-game/src/main/java/com/maxwellhaley/war/game/War.java
+++ war-game/src/main/java/com/maxwellhaley/war/game/War.java
@@ -1,43 +1,27 @@
 package com.maxwellhaley.war.game;
 
-import java.io.IOException;
 import java.util.Random;
-
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.gui2.DefaultWindowManager;
-import com.googlecode.lanterna.gui2.EmptySpace;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.maxwellhaley.war.game.window.InstructionWindow;
-import com.maxwellhaley.war.game.window.MainMenu;
 
 public class War {
 
   public static void main(String[] args) {
-    Terminal terminal = null;
-    try {
-      terminal = new DefaultTerminalFactory().createTerminal();
-      Screen screen = new TerminalScreen(terminal);
-      screen.startScreen();
+    clearScreen();
+    System.out.println("▄█     █▄     ▄████████    ▄████████ \n" +
+            "███     ███   ███    ███   ███    ███ \n" +
+            "███     ███   ███    ███   ███    ███ \n" +
+            "███     ███   ███    ███  ▄███▄▄▄▄██▀ \n" +
+            "███     ███ ▀███████████ ▀▀███▀▀▀▀▀   \n" +
+            "███     ███   ███    ███ ▀███████████ \n" +
+            "███ ▄█▄ ███   ███    ███   ███    ███ \n" +
+            " ▀███▀███▀    ███    █▀    ███    ███ \n" +
+            "                           ███    ███ \n" +
+            "\n" +
+            "");
+    System.out.println("Welcome to WAR! Pick a game mode:");
+  }
 
-      MultiWindowTextGUI gui = new MultiWindowTextGUI(screen,
-              new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
-      gui.addWindowAndWait(new MainMenu());
-      gui.addWindowAndWait(new InstructionWindow());
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (terminal != null) {
-        try {
-          terminal.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
+  private static void clearScreen() {
+    System.out.print("\033[2J");
   }
 
   private String victoryVerbs() {
